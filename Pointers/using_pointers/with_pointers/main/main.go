6b91914ea924227492a9b338d49342dc81bc4fed
Ravikanth Garimella <rgarimella@linkedin.com>
2017-09-03 04:40:45 +0530

    Adding notes for Pointers

--- Pointers/using_pointers/with_pointers/main/main.go
+++ Pointers/using_pointers/with_pointers/main/main.go
@@ -19,3 +19,31 @@ func main(){
     fmt.Printf("After modifying: Address of x = %d\n",&x)   
 
 }
+
+/* Notes:
+
+1. In the above program, we assign a value of x to 42. We are trying to modify the value of x through a function, and print the value of x after modification.
+
+2. After assignment, we Print the value of x , which will return 42, and also print the address which is for example : 1234567890
+
+3. We then call the modify function, which passes the memory address of x as an argument to the function.
+
+4. Function modify takes the pointer as an input.
+
+5. While entering the function, the value of z, which is *z, will be 42.
+
+6. The memory address of z will be 1234567890, which is the same as given for x in the main function.
+
+7. We change the value of *z, which implies *z => x (Please refer to basic Pointers if you do not understand it.) to 0
+
+8. After changing, the value of *z becomes 0.
+
+9. The memory address of z becomes : 1234567890.
+
+10. In the main function, after modification, the value of x becomes 0.
+
+11. The memory address of x will be 1234567890.
+
+12. Since we are passing the address itself, instead of passing the value of x, the function was able to recognise, which value to be changed, hence after modification, we see the modified value of x, which satisfies our requirement.
+
+*/
--- Pointers/using_pointers/without_pointers/main/main.go
+++ Pointers/using_pointers/without_pointers/main/main.go
@@ -19,3 +19,34 @@ func main(){
  fmt.Printf("After modifying teh value of x is %d\n",x)
  fmt.Printf("After modifying the address of x is %d\n",&x)
 }
+
+
+/* Notes:
+
+1. In the above program, we assign a value of x to 5. We are trying to modify the value of x through a function, and print the value of x after modification.
+
+2. After assignment, we Print the value of x , which will return 5, and also print the address which is for example : 1234567890
+
+3. We call the modify function, which the argument of x. i.e. We pass the argument 5 into the modify function.
+
+4. In the function, we call the variable as z, for no confusion.
+
+5. When entering the function the value of z will be 5.
+
+6. The address of z will be different, the address will become 1234567891.
+
+7. We set the value of z to 0, i.e. ideally the value of x should become 0. But since the memory address of both the variables are different, there will be no change to the value, even after modification.
+
+8. After modification, the value of z will be 0.
+
+9. The memory address of z after changes will be 1234567891.
+
+10. In the main function, after the modify function is called, the value of x will still be 5.
+
+11. The memory address of x will still be 1234567890.
+
+12. Hence the program doesn't satisfy our requirement.
+
+13. With the help of Pointers, we would be able to solve this problem
+
+*/
