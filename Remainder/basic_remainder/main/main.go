b2864a6cf684e9611dd0d852af250c2a94f8427e
Ravikanth Garimella <rgarimella@linkedin.com>
2017-09-03 05:08:17 +0530

    Remainder

---
+++ Remainder/basic_remainder/main/main.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+    x := 13 % 2
+    fmt.Printf("The remainder of 13 and 2 is %d\n",x)
+}
+
+/* Notes:
+
+1. This Program is mainly used for finding the remainder after dividing 2 variables.
+
+2. Remainder can be found with the operation : %
+
+3. Initially, we are trying to find the remainder of 13 and 2, which is 1, and printing the remainder value.
+
+*/
