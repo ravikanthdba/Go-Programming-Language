0558fb7e23a9fcecd929e85f4450b811636e72b0
Ravikanth Garimella <rgarimella@linkedin.com>
2017-09-02 13:09:50 +0530

    adding notes on referencing and dereferencing

--- Pointers/referencing_dereferencing/main/main.go
+++ Pointers/referencing_dereferencing/main/main.go
@@ -16,3 +16,25 @@ func main(){
 //    fmt.Printf("The value of *(&y) is ",*(&y))
 
 }
+
+/* Notes:
+
+1. The above program depicts referencing and dereferencing of the Pointer.
+
+2. We initialize an integer variable x to 42.
+
+3. We initialize a pointer variable y to memory location of x.
+
+4. The value of x becomes 42.
+
+5. The value of y will become address of x.
+
+6. The value of *y becomes the value of x, which is 42.
+
+7. The value of &y becomes the memory address of y.
+
+8. The value of &*y becomes the address of x, as eventually &*y => &x, which is address of x.
+
+9. The other values like **y, &&y , &*y and *&y throws an error message.
+
+*/
